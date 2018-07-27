55ee4371406e20de19a4a2c0eca677c1a14c6211
mreitschuster <41780255+mreitschuster@users.noreply.github.com>
2018-07-27 16:14:14 +0530

    Update lossAggregator.R

--- lossAggregator.R
+++ lossAggregator.R
@@ -1,7 +1,6 @@
 # Risk Of Life
 # Implementation of the Loss Aggregator
 # input='data_monetaryLoss', output=''
-# written by Markus Reitschuster on 20.7.18
 
 Loss <- function(data_monetaryLoss)
 {
