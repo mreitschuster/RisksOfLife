4797d06c7bd1292911f7bb781647afe1b4dda332
mreitschuster <41780255+mreitschuster@users.noreply.github.com>
2018-07-27 18:09:29 +0530

    Delete lossAggregator.R

--- lossAggregator.R
+++
@@ -1,8 +0,0 @@
-# Risk Of Life
-# Implementation of the Loss Aggregator
-# input='data_monetaryLoss', output=''
-
-Loss <- function(data_monetaryLoss)
-{
-# still need to figure out what to do here
-}
