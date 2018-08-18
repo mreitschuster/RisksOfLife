c320e2148f6d4df0e57c1e581540bcb6733cfa2e
mreitschuster <41780255+mreitschuster@users.noreply.github.com>
2018-08-18 19:06:37 +0200

    igraph integration first try

--- R/Graph_Build.R
+++ R/Graph_Build.R
@@ -24,3 +24,31 @@ Graph_Build <- function(){
     Rgraph=Rgraph[-1,] # remove the NA row
     return(Rgraph)
 }
+
+
+#' Convert the RiskOfLife specific graph format to igraph
+#' 
+#' 
+
+#' @return igraph The object that represents the graph structure / the causal chain
+#' @export 
+#' 
+Graph_convert_to_igraph<-function(Rgraph, onlyActive){
+  #plot(graph_from_literal(a--+b, b+--c))
+  #g4 <- set_graph_attr(g4, "name", "Email Network")
+  
+  if (FALSE){
+    onlyActive=TRUE
+  }
+  Nodes=as.data.frame(unique(c(Rgraph[(!onlyActive | as.logical(Rgraph[,'Active'])),"In"],Rgraph[(!onlyActive | as.logical(Rgraph[,'Active'])),"Out"])))
+  Edges=as.data.frame(Rgraph[(!onlyActive | as.logical(Rgraph[,'Active'])),])
+  Edges$DataType=NULL
+  #colnames(Edges)[colnames(Edges) %in% "In"]="from"
+  #colnames(Edges)[colnames(Edges) %in% "Out"]="to"
+
+  g <- graph_from_data_frame(Edges, directed=TRUE, vertices=Nodes)
+  return(g)
+  
+  
+  
+}
