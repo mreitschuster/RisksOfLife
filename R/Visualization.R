d7afd4fbb8d2deb7ad297c390c658094ce2e7dbe
username <user@ma900s>
2018-08-18 20:34:39 +0200

    dead end

---
+++ R/Visualization.R
@@ -0,0 +1,63 @@
+x#' Build causality Graph for RisksOfLife
+#' 
+#' This will hopefully be replaced by a load functionality on existing graphs, together with a more intuitive way to create the graphs.
+#' 
+
+#' @return Rgraph The object that represents the graph structure / the causal chain
+#' @export 
+#' 
+
+library(shiny)
+
+
+ui <- basicPage(
+  
+  plotOutput("plot1", click = "plot_click"),
+  
+  verbatimTextOutput("info")
+  
+)
+
+
+
+server <- function(input, output) {
+  
+  output$plot1 <- renderPlot({
+    
+    nodes <- data.frame(name=c("Risk", "Accident", "Death", "LoE","CoC","Loss"),
+                        
+                        color=c("yellow", "red", "red", "green","green","blue"),
+                        
+                        size=rep(40,6))
+    
+    edges <- data.frame(   bla=c("Risk",     "Risk",  "Accident", "Death",  "Accident", "LoE", "CoC"),
+                           
+                           
+                           
+                           blalba=c("Accident", "Death", "Death",    "LoE",    "CoC",      "Loss","Loss"),
+                           
+                           stuff=c("data_none", "data_none", "data_disability",    "data_death",    "data_disability",      "data_money","data_money")
+                           
+                           
+                           
+    )
+    
+    g <- graph_from_data_frame(edges, directed=TRUE, vertices=nodes)
+    
+    plot.igraph(g, canvas.width = 450, canvas.height = 450)
+    
+  })
+  
+  
+  
+  output$info <- renderText({
+    
+    paste0("x=", input$plot_click$x, "\ny=", input$plot_click$y)
+    
+  })
+  
+}
+
+
+
+shinyApp(ui, server)
