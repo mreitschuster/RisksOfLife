fd7d81a8f210f037d33d38581169c7a79ca15171
hendl <hendl@ma3900x>
2020-12-21 13:18:25 +0100

    visNetwork nodeselection via visoptions. edges not selected properly

--- scripts/Visualization_RoL_visnetwork.R
+++ scripts/Visualization_RoL_visnetwork.R
@@ -15,15 +15,15 @@ ui <- fluidPage(
   titlePanel("Risks of Life"),
   sidebarLayout(
     sidebarPanel(
-      textInput("PathFilter","Filter"),
-      uiOutput("ui_SelectPaths"),
-      uiOutput("ui_SelectNode")
+#      textInput("PathFilter","Filter"),
+      uiOutput("ui_SelectPaths")#,
+      #uiOutput("ui_SelectNode")
       
     ),
     mainPanel(
-      visNetworkOutput("network", height="1200px")  #,
-      #br(),br(),
-      #verbatimTextOutput("info")
+      visNetworkOutput("network", height="1000px")  ,
+      br(),br(),
+      verbatimTextOutput("info")
     )
   )  
 )
@@ -52,47 +52,116 @@ server <- function(input, output) {
   path_results=Graph_calculate_paths(paths)
   
   # get paths as strings
-  names_of_paths=vector(length = length(paths))
+  paths.id=paste("Path",1:length(paths),sep = "")
+  paths.name=vector(length = length(paths))
   for (i in 1:length(paths)){
-    names_of_paths[i]=paste(paths[[i]][,1],collapse=' -> ')
-    names_of_paths[i]=paste(names_of_paths[i],paths[[i]][nrow(paths[[i]]),3],sep =' -> ')
+    paths.name[i]=paste(paths[[i]][,1],collapse=' -> ')
+    paths.name[i]=paste(paths.name[i],paths[[i]][nrow(paths[[i]]),3],sep =' -> ')
   }
 
   # get static igraph object
   Edges=Graph_convert_to_visNetwork_edges(Rgraph,T)
   Nodes=Graph_convert_to_visNetwork_nodes(Rgraph,T)
+  
+  #use the paths as group (not visgroup) names - add attribute paths to Nodes
+  Nodes$paths=rep(NA,nrow(Nodes))
+  for (i in 1:length(paths)){
+    for (j in 1:nrow(Nodes)){
+      if(Nodes$id[j] %in% unique(rbind(paths[[i]][,"In"],paths[[i]][,"Out"]))){
+        if(is.na(Nodes$paths[j])){
+          Nodes$paths[j]=paths.id[[i]]
+        }else{
+          Nodes$paths[j]=paste(Nodes$paths[j],",",paths.id[[i]])
+        }
+      }
+    }
+  }  
 
+  #use the paths as group (not visgroup) names - add attribute paths to Edges
+  Edges$paths=rep(NA,nrow(Edges))
+  for (i in 1:length(paths)){
+    for (j in 1:nrow(Edges)){
+      if(any((paths[[i]][,"In"] %in%  Edges[j,"from"]) & (paths[[i]][,"Out"] %in%  Edges[j,"to"]))){
+        if(is.na(Edges$paths[j])){
+          Edges$paths[j]=paths.id[[i]]
+        }else{
+          Edges$paths[j]=paste(Edges$paths[j],",",paths.id[[i]])
+        }
+      }
+    }
+  }  
+  
   
 ######## build inputs ###########################################################################
   output$ui_SelectPaths <- renderUI({
-    checkboxGroupInput("SelectPath","Select Paths",choices=names_of_paths,selected = names_of_paths, inline=F)
+    checkboxGroupInput("SelectPath","Select Paths",choices=paths.name,selected = paths.name, inline=F)
   })
 
-  output$ui_SelectNode <- renderUI({
-    radioButtons("SelectNode","Select Node",choices=as.matrix(Nodes),selected = "Monetary_Loss", inline=F)
-  })  
+  #output$ui_SelectNode <- renderUI({
+   # radioButtons("SelectNode","Select Node",choices=as.matrix(Nodes$label),selected = input$current_node_id[[1]], inline=F)
+  #})  
   
 ######## output ###########################################################################  
   output$network <- renderVisNetwork({
-
-    #visNetwork(Nodes, Edges, width = "100%")
+    ###select by Node
+    if (FALSE){
+      visNetwork(Nodes, Edges) %>%
+        visEdges(arrows='to',smooth=F)%>%
+        #  visOptions(manipulation = TRUE)  %>%
+        visPhysics(solver='barnesHut') %>% # 'barnesHut', 'repulsion', 'hierarchicalRepulsion', 'forceAtlas2Based'. 
+        visPhysics(maxVelocity=50) %>% 
+        visPhysics(minVelocity=0.1) %>% 
+        visPhysics(timestep=0.5) %>% 
+        visPhysics(stabilization=list(
+          enabled = TRUE,
+          onlyDynamicEdges = TRUE,
+          fit = TRUE)) %>% 
+        visPhysics(enabled=F)  %>% 
+        visOptions(nodesIdSelection = TRUE)%>% 
+        #visInteraction(hover = TRUE) %>%      # needed for input current_node_id
+        #visEvents(hoverNode = "function(nodes) {Shiny.onInputChange('current_node_id', nodes);;}") # needed for input current_node_id
+        visInteraction(selectable = TRUE) %>%      # needed for input current_node_id
+        visEvents(selectNode = "function(nodes) {Shiny.onInputChange('current_node_id', nodes);;}") # needed for input current_node_id
+    }
+    
+    ### select by path
+    if (FALSE){
+      visNetwork(Nodes, Edges) %>%
+        visEdges(arrows='to',smooth=F)%>%
+        visPhysics(solver='barnesHut') %>% # 'barnesHut', 'repulsion', 'hierarchicalRepulsion', 'forceAtlas2Based'. 
+        visPhysics(maxVelocity=50) %>% 
+        visPhysics(minVelocity=0.1) %>% 
+        visPhysics(timestep=0.5) %>% 
+        visPhysics(stabilization=list(
+          enabled = TRUE,
+          onlyDynamicEdges = TRUE,
+          fit = TRUE)) %>% 
+        visPhysics(enabled=F)  %>% 
+        visOptions(selectedBy = list(variable = "paths", multiple = T)) #%>%
+    }
+    
     visNetwork(Nodes, Edges) %>%
-    visEdges(arrows='to',smooth=F)%>%
-    visOptions(manipulation = TRUE)  %>%
-    visPhysics(solver='barnesHut') %>% # 'barnesHut', 'repulsion', 'hierarchicalRepulsion', 'forceAtlas2Based'. 
-    visPhysics(maxVelocity=50) %>% 
-    visPhysics(minVelocity=0.1) %>% 
-    visPhysics(timestep=0.5) %>% 
-    visPhysics(stabilization=list(
-      enabled = TRUE,
-      onlyDynamicEdges = TRUE,
-      fit = TRUE)) %>% 
-    visPhysics(enabled=F)
- #   visOptions(nodesIdSelection = TRUE)
-  })
+      visEdges(arrows='to',smooth=F)%>%
+      visPhysics(solver='barnesHut', # 'barnesHut', 'repulsion', 'hierarchicalRepulsion', 'forceAtlas2Based'. 
+                maxVelocity=50,
+                minVelocity=0.1,
+                timestep=0.5,
+                stabilization=list(
+                  enabled = TRUE,
+                  onlyDynamicEdges = TRUE,
+                  fit = TRUE),
+                enabled=F)  %>% 
+      visOptions(selectedBy = list(variable = "paths", multiple = T, highlight = T),
+                 nodesIdSelection = list(enabled = T, useLabels = T))   %>% 
+      visInteraction(selectConnectedEdges=F)
+    
+
+    
+    })
   
   output$info <- renderText({
-    paste0("x=", input$plot_click$x, "\ny=", input$plot_click$y)
+    #paste0("x=", input$plot_click$x, "\ny=", input$plot_click$y)
+    paste0("Node is ",input$current_node_id)
   })
   
 }
